4e952eaaef3a0169f9ec00648d106d7b5b9f4272
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-03 23:59:10 -0500

    useful comments

--- lib/enumerable_methods.rb
+++ lib/enumerable_methods.rb
@@ -1,5 +1,6 @@
 module Enumerable
   def my_each
+    # Guard, use as enum
     return enum_for :my_each unless block_given?
 
     for value in self
@@ -8,6 +9,7 @@ module Enumerable
   end
 
   def my_each_with_index
+    # Guard, use as enum
     return enum_for :my_each_with_index unless block_given?
 
     self.my_each.zip(0..Float::INFINITY) { |value, i| yield value, i }
@@ -16,7 +18,10 @@ module Enumerable
   def my_select
     return enum_for :my_select unless block_given?
 
-    Enumerator.new { |y| self.my_each { |item| y << item if yield item } }.to_a
+    Enumerator.new do |y|
+
+      self.my_each { |item| y << item if yield item } # for each item ... keep if yield (item)
+    end.to_a # return as array
   end
 
   def my_all?
@@ -37,31 +42,33 @@ module Enumerable
     true
   end
 
-  def my_count(*match)
-    count = 0
-    use_match, match = match.length == 1, match[0]
-    self.my_each do |value|
-      next if block_given? and not yield(value)
-      next if use_match and match != value
-      count += 1
-    end
-    count
+  def my_count(item=:NONE)
+    is_item = lambda { |value| item==:NONE or item==value } # match with item arg or true if arg empty
+    is_match = lambda { |value| block_given? ? yield(value) : is_item.call(value) } # match with yield or item arg
+
+    # start 0, increment if is match otherwise continue
+    self.my_inject(0) { |count, value| is_match.call(value) ? count + 1 : count }
   end
 
   def my_map
+    # Guard, use as enum
     return enum_for :my_map unless block_given?
 
-    Enumerator.new { |y| self.my_each { |value| y << yield(value) } }.to_a
+    Enumerator.new do |y|
+      self.my_each { |value| y << yield(value) } # for each item ... yield item
+    end.to_a # return as array
   end
 
   def my_inject(initial=nil, sym=nil)
-    # swap arguments if not block depending on symbol
+    # swap arguments if depending on missing symbol
     (initial, sym = sym, initial) if not block_given? and sym === nil
 
     enum = self.my_each # create enumerator
-    left = initial || enum.next # use initial or next
+    result = initial || enum.next # use initial or next
 
-    loop { left = block_given? ? yield(left, enum.next) : left.send(sym, enum.next) }
-    left
+
+    # for each value ... result = yield (result, next)
+    loop { result = block_given? ? yield(result, enum.next) : result.send(sym, enum.next) }
+    result
   end
 end
